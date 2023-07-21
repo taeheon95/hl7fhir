03e1638fb1f77066cb2b07eca23f2d822746c4a5
taeheon95 <dalo952003@gmail.com>
2023-07-21 23:00:18 +0900

    파이어 모델 컨버터 추가

---
+++ src/main/java/com/taeheon/fhir/common/fhir/FhirConverter.java
@@ -0,0 +1,52 @@
+package com.taeheon.fhir.common.fhir;
+
+import ca.uhn.fhir.parser.IParser;
+import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.springframework.http.HttpInputMessage;
+import org.springframework.http.HttpOutputMessage;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.HttpMessageNotWritableException;
+
+import java.io.IOException;
+import java.util.List;
+
+public class FhirConverter implements HttpMessageConverter<IBaseResource> {
+
+    private final IParser parser;
+    private final List<MediaType> supportedMediaTypeList;
+
+    public FhirConverter(IParser parser, MediaType fhirMediaType, MediaType normalMediaType) {
+        this.parser = parser;
+        this.supportedMediaTypeList = List.of(new MediaType[]{fhirMediaType, normalMediaType});
+    }
+
+    @Override
+    public boolean canRead(Class<?> clazz, MediaType mediaType) {
+        return clazz.isAssignableFrom(IBaseResource.class)
+                && supportedMediaTypeList.stream().anyMatch(type -> type.equals(mediaType));
+    }
+
+    @Override
+    public boolean canWrite(Class<?> clazz, MediaType mediaType) {
+        return clazz.isAssignableFrom(IBaseResource.class)
+                && supportedMediaTypeList.stream().anyMatch(type -> type.equals(mediaType));
+    }
+
+    @Override
+    public List<MediaType> getSupportedMediaTypes() {
+        return supportedMediaTypeList;
+    }
+
+    @Override
+    public IBaseResource read(Class<? extends IBaseResource> clazz, HttpInputMessage inputMessage) throws IOException, HttpMessageNotReadableException {
+        return null;
+    }
+
+    @Override
+    public void write(IBaseResource iBaseResource, MediaType contentType, HttpOutputMessage outputMessage) throws IOException, HttpMessageNotWritableException {
+        outputMessage.getHeaders().setContentType(supportedMediaTypeList.get(0));
+        outputMessage.getBody().write(parser.encodeResourceToString(iBaseResource).getBytes());
+    }
+}
